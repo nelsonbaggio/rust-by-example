ede5be8ee6fd6702cdd53c0c55ba41dcc563e70a
Nelson Baggio <nelson.baggio@gmail.com>
2020-07-20 12:42:48 -0300

    Formatted print: Display

--- src/formatted-print.rs
+++ src/formatted-print.rs
@@ -1,6 +1,9 @@
 use std::fmt;
 
 fn main() {
+    
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    
     #[derive(Debug)]
     struct Structure(i32);
 
@@ -9,11 +12,25 @@ fn main() {
             write!(f, "({})", self.0)
         }
     }
-
-    println!("My name is {0}, {1} {0}", "Bond", "James");
+    
     println!("This struct `{:#?}` won't print...", Structure(3));
     println!("This struct `{}` won't print...", Structure(3));
     
     let pi = 3.141592;
     println!("Pi is roughly {0:.3}", pi);
+
+    #[derive(Debug)]
+    struct Complex {
+        real: f64,
+        imag: f64,
+    }
+    
+    impl fmt::Display for Complex {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{} + {}i", self.real, self.imag)
+        }
+    }
+
+    println!("Display: {}", Complex { real: 3.3, imag: 7.2 });
+    println!("Debug: {:?}", Complex { real: 3.3, imag: 7.2 });
 }
